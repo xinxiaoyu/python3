a8cb64ab0935ee721d6978ec1f437f7da3b539a2
xinxiaoyu <xinxiaoyu@users.noreply.github.com>
2018-01-17 15:54:40 +0800

    Update test_ip_ping.py

--- test_ip_ping.py
+++ test_ip_ping.py
@@ -1,13 +1,17 @@
 import subprocess
 import  mail2
+import time
 
 def testvpn():
     p = subprocess.Popen('ping -n 1 47.90.1.81')
     p.wait()
 
     if p.poll() == 0:
-        mail2.send_mail(from_addr='1248247511@qq.com', password, to_addr='1248247511@qq.com')
+        mail2.send_mail(from_addr='1248247511@qq.com',password='',to_addr='1248247511@qq.com',subject='yes',content='This is a test mail!')
     else:
-        mail2.send_mail(from_addr='1248247511@qq.com', password, to_addr='1248247511@qq.com')
+        mail2.send_mail(from_addr='1248247511@qq.com', password='', to_addr='1248247511@qq.com',
+                        subject='no', content='This is a test mail!')
 
-testvpn()
+while True:
+    testvpn()
+    time.sleep(3600)
