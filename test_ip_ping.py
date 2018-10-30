3dd231340d3d42901fbe1d397c73aa426b66feb2
xinxiaoyu <xinxiaoyu@users.noreply.github.com>
2018-10-31 00:31:57 +0800

    Update test_ip_ping.py

--- test_ip_ping.py
+++ test_ip_ping.py
@@ -1,17 +1,33 @@
 import subprocess
-import  mail2
+import qmail
 import time
 
+
+def sendmail1():
+    qmail.send_mail(from_addr='1248247511@qq.com', password='', to_addr='1248247511@qq.com',
+                    subject='lang_mail_yes', content='This is a test mail!')
+
+def sendmail2():
+    qmail.send_mail(from_addr='1248247511@qq.com', password='', to_addr='1248247511@qq.com',
+                    subject='no', content='This is a test mail!')
+
+a = 0
+
 def testvpn():
-    p = subprocess.Popen('ping -n 1 47.90.1.81')
+    p = subprocess.Popen(['ping', '-c', '1', '103.210.23.200'])
     p.wait()
 
+    global a
     if p.poll() == 0:
-        mail2.send_mail(from_addr='1248247511@qq.com',password='',to_addr='1248247511@qq.com',subject='yes',content='This is a test mail!')
+        a += 1
+        if a == 1:
+            sendmail1()
+        else:
+            print('pass')
     else:
-        mail2.send_mail(from_addr='1248247511@qq.com', password='', to_addr='1248247511@qq.com',
-                        subject='no', content='This is a test mail!')
+        sendmail2()
+        a = 0
 
 while True:
     testvpn()
-    time.sleep(3600)
+    time.sleep(60)
