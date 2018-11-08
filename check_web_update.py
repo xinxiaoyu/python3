d845b6b32bf75a227891a4e90f44ca7d35352793
xinxiaoyu <xinxiaoyu@users.noreply.github.com>
2018-11-09 06:26:11 +0800

    Update check_web_update.py

--- check_web_update.py
+++ check_web_update.py
@@ -1,31 +1,42 @@
-def download_index2():
-    import urllib.request
-    index=urllib.request.urlopen('http://www.yinwang.org')
-    content=index.read()
+"""python3"""
 
-    filename=open('/tmp/index.html2','wb')
+import hashlib
+import time
+
+from qmail import send_mail
+
+
+def download_index():
+    import urllib.request
+    index = urllib.request.urlopen('http://www.yinwang.org')
+    content = index.read()
+    filename = open('/tmp/index.html', 'wb')
     filename.write(content)
     filename.close()
 
+
+def html1():
+    global html1
+    html1 = hashlib.md5(open('/tmp/index.html', 'rb').read()).hexdigest()
+
+
+def html2():
+    global html2
+    html2 = hashlib.md5(open('/tmp/index.html', 'rb').read()).hexdigest()
+
+
 def diff_md5():
-    import hashlib
-    html1=hashlib.md5(open('/tmp/index.html','rb').read()).hexdigest()
-    html2=hashlib.md5(open('/tmp/index.html2','rb').read()).hexdigest()
     if html1 == html2:
         pass
     else:
-        do_action()
+        send_mail('1248247511@qq.com', '', '1248247511@qq.com', subject="yingwang.org has update")
 
-def do_action():
-    import os
-    os.remove('/tmp/index.html')
-    os.rename('/tmp/index.html2','/tmp/index.html')
-    import qmail
-    qmail.send_mail('','','',subject="")
 
-if __name__ == '__main__':
-    import time
-    while True:
-        download_index2()
-        diff_md5()
-        time.sleep(60)
+download_index()
+html1()
+
+while True:
+    time.sleep(60)
+    download_index()
+    html2()
+    diff_md5()
