1defb813608a1871f4de57c027526c848f470ae9
xinxiaoyu <xinxiaoyu@users.noreply.github.com>
2021-03-15 16:14:02 +0800

    Update test_ip_ping.py

--- test_ip_ping.py
+++ test_ip_ping.py
@@ -1,33 +1,26 @@
-import subprocess
-import qmail
-import time
+import os
+from urllib import request
+import json
+
+ding_url = 'https://oapi.dingtalk.com/robot/send?access_token='
 
 
-def sendmail1():
-    qmail.send_mail(from_addr='1248247511@qq.com', password='', to_addr='1248247511@qq.com',
-                    subject='lang_mail_yes', content='This is a test mail!')
+def notice():
+    params = {
+        "msgtype": "text",
+        "text": {"content": "测试: ping成功"}
+    }
+    params = bytes(json.dumps(params), 'utf8')
+    headers = {'Accept-Charset': 'utf-8', 'Content-Type': 'application/json'}
 
-def sendmail2():
-    qmail.send_mail(from_addr='1248247511@qq.com', password='', to_addr='1248247511@qq.com',
-                    subject='no', content='This is a test mail!')
+    req = request.Request(url=ding_url, data=params, headers=headers, method='POST')
+    request.urlopen(req).read()
 
-a = 0
 
-def testvpn():
-    p = subprocess.Popen(['ping', '-c', '1', '103.210.23.200'])
-    p.wait()
-
-    global a
-    if p.poll() == 0:
-        a += 1
-        if a == 1:
-            sendmail1()
-        else:
-            print('pass')
+ip_list = ['174.137.63.74', 'www.baidu.com']
+cmd = 'ping -c 1'
+for i in ip_list:
+    if os.system('ping -n 1 ' + i):
+        pass
     else:
-        sendmail2()
-        a = 0
-
-while True:
-    testvpn()
-    time.sleep(60)
+        notice()
